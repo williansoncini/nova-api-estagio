62c6898a08aaf1e03267d64b245eabc657d76804
Willian <willian_soncini@hotmail.com>
2021-09-15 19:25:40 -0300

    Mudança de rotas

--- server/route/tableRoute.js
+++ server/route/tableRoute.js
@@ -2,32 +2,32 @@ const express = require('express');
 const router = express.Router();
 const tableService = require('../service/tableService')
 
-router.post('/table/', async function (req, res){
+router.post('/tables/', async function (req, res){
     const data = req.body;
     const table = await tableService.saveTable(data);
     return res.json(table);
 });
 
-router.put('/table/:id', async function(req, res){
+router.put('/tables/:id', async function(req, res){
     const id = req.params.id;
     const data = req.body;
     const alterTable = await tableService.alterTable(id, data);
     return res.json(alterTable);
 });
 
-router.delete('/table/:id', async function(req, res){
+router.delete('/tables/:id', async function(req, res){
     const id = req.params.id;
     await tableService.deleteTable(id);
     return res.json('sucess!')
 })
 
-router.get('/table/:id', async function(req, res){
+router.get('/tables/:id', async function(req, res){
     const id = req.params.id;
     const table = await tableService.findTableById(id);
     return res.json(table);
 })
 
-router.get('/table/', async function(req, res){
+router.get('/tables/', async function(req, res){
     const tables = await tableService.getTables();
     return res.json(tables);
 })
--- server/test/tabela.test.js
+++ server/test/tabela.test.js
@@ -20,54 +20,54 @@ const dataTable = function(){
 
 test("Cadastro de tabela", async function(){
     const data = dataTable();
-    const response = await request('http://localhost:3000/table','post',data);
+    const response = await request('http://localhost:3000/tables','post',data);
     const newTable = response.data;
-    const responseGetTable = await request(`http://localhost:3000/table/${newTable.id}`,'get');
+    const responseGetTable = await request(`http://localhost:3000/tables/${newTable.id}`,'get');
     const getTable = responseGetTable.data;
     // const getTable = await tableService.findTableById(newTable.id);
     expect(newTable.id).toBe(getTable.id);
     expect(newTable.nome).toBe(getTable.nome);
-    await request(`http://localhost:3000/table/${newTable.id}`,'delete');
+    await request(`http://localhost:3000/tables/${newTable.id}`,'delete');
 });
 
 test("Alteração de tabela", async function(){
-    const responseNewTable = await request('http://localhost:3000/table','post',dataTable());
+    const responseNewTable = await request('http://localhost:3000/tables','post',dataTable());
     const newTable = responseNewTable.data;
 
     const newData = dataTable();
-    const responseAlterTable = await request(`http://localhost:3000/table/${newTable.id}`,'put', newData);
+    const responseAlterTable = await request(`http://localhost:3000/tables/${newTable.id}`,'put', newData);
     const alterTable = responseAlterTable.data;
     expect(alterTable.nome).toBe(newData.nome);
-    await request(`http://localhost:3000/table/${newTable.id}`,'delete');
+    await request(`http://localhost:3000/tables/${newTable.id}`,'delete');
 });
 
 test("Apagar tabela", async function(){
-    const responseNewTable = await request('http://localhost:3000/table','post',dataTable());
+    const responseNewTable = await request('http://localhost:3000/tables','post',dataTable());
     const newTable = responseNewTable.data;
-    await request(`http://localhost:3000/table/${newTable.id}`,'delete');
+    await request(`http://localhost:3000/tables/${newTable.id}`,'delete');
     
-    const responseGetTable = await request(`http://localhost:3000/table/${newTable.id}`,'get');
+    const responseGetTable = await request(`http://localhost:3000/tables/${newTable.id}`,'get');
     const getTable = responseGetTable.data;
 
     expect(getTable).toBe(null);
 });
 
 test("Pegar apenas uma tabela", async function(){
-    const responseNewTable = await request('http://localhost:3000/table','post',dataTable());
+    const responseNewTable = await request('http://localhost:3000/tables','post',dataTable());
     const newTable = responseNewTable.data;
-    const responseGetTable = await request(`http://localhost:3000/table/${newTable.id}`,'get');
+    const responseGetTable = await request(`http://localhost:3000/tables/${newTable.id}`,'get');
     const getTable = responseGetTable.data;
     expect(getTable.id).toBe(newTable.id);
     expect(getTable.nome).toBe(newTable.nome);
-    await request(`http://localhost:3000/table/${newTable.id}`,'delete');
+    await request(`http://localhost:3000/tables/${newTable.id}`,'delete');
 })
 
 test("Pegar apenas uma tabela", async function(){
-    const responseNewTable = await request('http://localhost:3000/table','post',dataTable());
+    const responseNewTable = await request('http://localhost:3000/tables','post',dataTable());
     const newTable = responseNewTable.data;
-    const responseGetTables = await request('http://localhost:3000/table','get');
+    const responseGetTables = await request('http://localhost:3000/tables','get');
     const tables = responseGetTables.data;
     const lenght = tables.lenght;
     expect(lenght).not.toBe(0);
-    await request(`http://localhost:3000/table/${newTable.id}`,'delete');
+    await request(`http://localhost:3000/tables/${newTable.id}`,'delete');
 })
