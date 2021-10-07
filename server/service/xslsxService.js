a2f0a67660cd7910806f5cbbdb5a1ed941a55ff7
Willian <willian_soncini@hotmail.com>
2021-10-06 21:19:20 -0300

    Adicionando uploadFileService

--- server/service/xslsxService.js
+++ server/service/xslsxService.js
@@ -11,44 +11,15 @@ exports.makeMatrixWithDataXlsx = async function(filePath){
         const rowsLength = rows.length
         const columnsLength = rows[0]._cells.length
 
-        // let matrizValues = new Array(rowsLength).fill('null').map(() => new Array(columnsLength).fill('null'))
         let matrizValues = makeMatrixWithNullValues(rowsLength, columnsLength)
-
         let rowsWithCells = getValidRowsWithCells(rows)
-        // let rowsWithCells = []
-        // for (let i=0; i < rows.length; i++){
-        //     try {
-        //         validRowWithCell = rows[i]._cells
-        //         if (validRowWithCell)
-        //             rowsWithCells.push(validRowWithCell)
-        //     } catch (error) {
-        //         console.log('Linha vazia')     
-        //     }
-        // }
-
         let cells = getCellsFromRows(rowsWithCells)
-        // let cells = []
-        // for(let i=0; i < rowsWithCells.length;i++){
-        //     let realCells = rowsWithCells[i]
-        //     for (let o=0; o < realCells.length;o++){
-        //         if (realCells[o])
-        //             cells.push(realCells[o])
-        //     }
-        // }
         matrizValues = fillMatrixWithData(cells,matrizValues)
-        // for(let i=0; i < cells.length; i++){
-        //     rowNumber = cells[i]._row._number -1
-        //     columnNumber = cells[i]._column._number -1
-        //     matrizValues[rowNumber][columnNumber] = cells[i].value
-        // }
 
-        console.log(matrizValues)
         return matrizValues
-        // return res.json(matrizValues)     
-
     } catch (error) {
         console.log(error)  
-        return res.status(500).json('Deu ruim! na abertura do arquivo')     
+        return res.status(500).json('Erro em makeMatrixWithDataXlsx')     
     }
 }
 
@@ -64,7 +35,7 @@ function getValidRowsWithCells(rows){
             if (validRowWithCell)
                 rowsWithCells.push(validRowWithCell)
         } catch (error) {
-            console.log('Erro em getValidRowsWithCells')     
+            
         }
     }
     return rowsWithCells
